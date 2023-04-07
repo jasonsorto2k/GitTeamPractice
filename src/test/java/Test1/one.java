60ea1775e417efd984e8b78243e7218dba0a8c78
jasonsorto2k <jasonsorto2k@gmail.com>
2023-04-07 17:56:46 -0400

    Thee statement of declaring name has be completed.

---
+++ src/test/java/Test1/one.java
@@ -0,0 +1,9 @@
+package Test1;
+
+public class one {
+
+    public static void main(String[] args) {
+        //Please captilize the j in jason
+        System.out.println("My name is jason");
+    }
+}
